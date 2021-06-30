5d19e6b9e915d6b0b7fbbb6658e0364c5ff74e24
manjuvnair16 <manjupillai16@gmail.com>
2021-06-30 23:44:37 +0100

    Update final-project

--- final-project/adventure_game.py
+++ final-project/adventure_game.py
@@ -13,17 +13,22 @@ class Monster:
         self.attack_style = attack_style
 
     def case_hit(self,avatar):
-        print(f"You hit the monster! 🗡  ✨ \n")
+        print('\033c')
+        print(f"You hit the monster! 👊  ✨ \n")
         self.hit_points = self.hit_points - avatar.HP
         if self.hit_points <= 0:
-            print(f"Hurray! Monster lost the fight! ✨")
+            input(f"Bravo! Monster Slayed! ✨\n\
+                    \nPress enter to roll a dice & move forward that many steps!  🎲  🐾 \n\
+                ")
         else:
-            print (f"✨  Monster took {avatar.HP} damage. Remaining health = {self.hit_points}\n")
+            input (f"✨  Monster took {avatar.HP} damage. Remaining health = {self.hit_points}! Need another blow to be slayed! \n\
+                    \nPress enter to continue!\n\
+                ")
       
 
 response = requests.get('https://api.osrsbox.com/monsters?where={"duplicate":false}')
 monsters_api_data = response.json()
-monster_images = ['👾','👻','👹','🎃','👣 👣','👺','💀','☠ 💀','👻','🦀','👀 🐕','👿 🐺','👽 🐺','👿 👹 👽 🐺']
+monster_images = ['👾','👻','👹','🎃','👣 👣','👺','💀','☠ 💀','👻','🦀','👀 🐕','👿 🐺','👽 🐺','👿 👽 🐺']
 MONSTERS_LIST = []
 i = 0
 for monster_item in monsters_api_data["_items"]:
@@ -60,40 +65,48 @@ class Avatar:
 
     
     def decide_to_view_stats(self,monster):
-        print (f"{'STATS'.center(70)}\n\
+        print('\033c')
+        input(f"{'STATS'.center(70)}\n\
                 \n{'-'*70}\
-                \nYou:{self.img.center(24)}{'| Health:'.center(10)}{self.HP} {'| Signature Move:'.center(18)} {self.signature_move}\n\
-                \nMonster:{monster.img.center(5)}{monster.name.center(15)}{'| Health:'.center(10)}{monster.hit_points} {'| Attack Type:'.center(18)} {monster.attack_style[0]}\n\
-              ")
+                \nYou:{self.img.center(24)}{'| Health:'.center(10)}{self.HP}\n\
+                \nMonster:{monster.img.center(5)}{monster.name.center(15)}{'| Health:'.center(10)}{monster.hit_points}\n\
+                \nPress enter to continue!\n\
+                ")
     
 
     def decide_to_attack(self,monster):
+        print('\033c')
         print(f"You decided to attack! 🗡\n")
         result = random.choice(ATTACK_RESULTS)
         if result == "Hit":
             monster.case_hit(self)
             self.score += 2
         elif result == "Miss":
-            print(f"Oops, you missed the monster! 😨 \n")
+            input(f"Oops, you missed the monster! 😨 \n\
+            \nPress enter to continue!\n\
+            ")
         else:
             self.case_hurt(monster)
 
 
     def decide_to_run(self,monster):
+        print('\033c')
         print(f"You decided to run! 🐾 🐾\n")
         action = random.randint(0,1)
         if action == 0:
             no_of_steps_forward = random.randint(1,5)
             self.steps += no_of_steps_forward
             self.moved_forward = True
-            print(f"You escaped, phew! 😰\n")
-            print(f"You managed to run {no_of_steps_forward} steps away from the monster!\n")
+            input(f"You escaped, phew! 😰  You managed to run {no_of_steps_forward} steps away from the monster!\n\
+                    \nPress enter to continue!\n\
+                ")
         else:
             self.case_hurt(monster)
             self.moved_forward = False
 
 
     def decide_to_use_pick_ups(self,user_choice):
+        print('\033c')
         if user_choice > 0:
             pick_up_obj = PICK_UP_LIST[user_choice - 1]
             pick_up_obj.action(self)
@@ -101,15 +114,21 @@ class Avatar:
             print(f"\nYou decided to not use any pick-ups!\n\
                     \nGoing Back!\n\
                 ")
-        
-    
+
+
     def case_hurt(self,monster):
+        print('\033c')
         print(f"Oops, The monster attacked you! You got hurt! 🤕 \n")
+            
         self.HP = self.HP - monster.hit_points
         if self.HP <= 0:
-            print(f"You have 0 health! You lost!  👎\n")
+            input(f"You have 0 health! You lost!  👎\n\
+                \nPress enter to continue!\n\
+                ")
         else:
-            print (f"You lost {monster.hit_points} health points 😰")
+            input(f"You lost {monster.hit_points} health points 😰\n\
+            \nPress enter to continue!\n\
+            ")
 
     
 AVATAR_LIST = []
@@ -131,18 +150,25 @@ class PickUp:
     def action(self,avatar):
         if self.name.lower().startswith("time saver"):
            avatar.steps += 3
-           print(f"\nYou've moved forward 3 steps\n")
+           input(f"\nYou've moved forward 3 steps 🐾 \n\
+                    \nPress enter to continue!\n\
+                ")
            avatar.moved_forward = True
-          
+           
         elif self.name.lower().startswith("magic potion"):
             avatar.HP += 30
-            print(f"\nYour HP has been increased by 30!\n")
+            input(f"\nYour health has been increased by 30! 💚\n\
+                    \nPress enter to continue!\n\
+                ")
             
         elif self.name.lower().startswith("treasure"):
             avatar.score +=20
-            print(f"\nYour score has been increased by 20!\n")
+            input(f"\nYour score has been increased by 20!\n\
+                    \nPress enter to continue!\n\
+                ")
             
         avatar.pick_ups_used.append(self.name.lower())
+
 
 PICK_UP_LIST = []
 with open('pick_up_data.csv','r') as file:
@@ -154,9 +180,9 @@ with open('pick_up_data.csv','r') as file:
 
 
 MILESTONES_FOR_PICK_UP = {
-                            'first_pick_up_after' : 3,
-                            'second_pick_up_after': 9,
-                            'third_pick_up_after' : 12
+                            'first_pick_up_after' : 5,
+                            'second_pick_up_after': 10
+                            #'third_pick_up_after' : 12
                           }
 
 MILESTONES_FOR_PICK_UP_LIST = list(MILESTONES_FOR_PICK_UP.values())
@@ -171,6 +197,7 @@ def create_divider(num_of_asterix):
 
 
 def display_start_menu():
+    print('\033c')
     return input(f"\n{create_divider(75)}\
                     \n{'✨ 🔥  Play Adventure Game? 🗡 ✨'.center(70)}\n\
                     \n{'Yes: Press y'.center(70)}\
@@ -180,17 +207,19 @@ def display_start_menu():
 
 
 def display_game_instructions():
+    print('\033c')
     input(f"\n{'📜  Instructions: 📜'.center(70)}\
             \n{'-'*75}\
-            \n{'📕  Game ends when HP decreases to 0'.ljust(70)}\n\
-            \n{'📗  Magic Potion helps to increase HP by 30'.ljust(70)}\n\
+            \n{'📕  Game ends when health decreases to 0'.ljust(70)}\n\
+            \n{'📗  Magic Potion helps to increase health by 30'.ljust(70)}\n\
             \n{'📙  Need to reach the end point to complete any level.'.ljust(70)}\n\
-            \n{'📘  Collect as many pick-ups, hit & slay as many lurking monsters to ⬆  your score.'.ljust(70)}\n\
+            \n{'📘  Collect pick-ups, hit & slay as many lurking monsters to ⬆  your score.'.ljust(70)}\n\
             \n\n{'Press enter to continue'.ljust(70)}\n\
         \n")
 
 
 def display_avatar_options():
+    print('\033c')
     return input(f"\n\n{'Choose Your Avatar:'.center(40)}\n\
                \n{'🐯'.center(10)}|{'🦁'.center(10)}|{'🐻'.center(10)}|{'🐱'.center(10)}\n\
                \n{'Tiger'.center(10)}|{'Lion'.center(10)}|{'Bear'.center(10)}|{'Fox'.center(10)}\n\
@@ -199,29 +228,33 @@ def display_avatar_options():
 
 
 def display_first_line_route():
-    return 'T  ' + '➡'* 3 + '🌳' * 4 + '🐞' + '🌳' * 2 + ' ' +'➡'* 10 + '⏳ ' + '➡'* 5 + '🌲' * 4 + '🐊' + '🌲' * 5 + ' ' +'➡'* 6 +'🌺' * 3 + ' ' + '➡'* 3 + ' ' * 5 + 'E'
-
+    path1 = '➡'* 2 + '🌳' + '🐞' + '🌳' + ' ' + '➡' + '✨' +'⏳' + '✨' + ' ' + '~'* 5 + '🌲' * 4 + '🐊' + '🌲' * 5 + ' ' + '➡'* 6 +'🌺' * 2 + '🌋'+ '🌺' + ' ' + '➡'* 3 + '👿' + ' ' + '➡' * 2 + '🌳'* 4 + '💩' + '🌳'* 3 + ' ' + '➡'* 5 + ' ' 
+    return path1
 
 def display_second_line_route():
-     return 'A  ' + '➡'* 2 + '🌳' * 4 + '🐍' + '🌳' * 2 + '  '+ '➡'* 4 + '👽  ' + '➡'* 6 +'🍯' + ' ' + '➡'* 5 + '🌴' * 4 + '🐙' + '🌲' * 5 +  ' ' + '➡'* 6 + '👾 ' + '➡'* 5 + ' ' * 5 + 'N'
-
+    path2 = '➡'* 2 + '🌳' * 4 + '🐍' + '🌳' * 2 + ' '+ '➡'* 4 + '👽' + ' ' + '➡'* 5 + '🌳' + '🦀' + '🌳' + ' ' + '✨' + '🍯' + '✨' + ' ' + '➡'* 5 + '🌴' * 4 + '🐙' + '🌲' * 5 +  ' ' + '~'* 5 + '👾' + ' ' + '➡'* 3 + '🌋' + ' ' + '➡'* 2 + ' '
+    return path2
 
 def display_third_line_route():
-     return 'R  ' + '➡'* 7 + '🌳' * 8 + '🐝 ' + '🌳' * 2 + ' ' + '➡'* 10 + '👺 ' + '➡'* 5 + '🌴' * 4 + '🐸' + '🌴' * 5 + ' ' + '➡'* 2 + '💰 ' + '➡'* 1 + ' ' * 4 + 'D'
+    path3 = '➡'* 7 + '🌳' * 3 + '🌹' * 2 + '🌳' * 2 + '🐝' + '🌳' * 2 + ' ' + '~'* 10 + '👺' + ' ' + '➡'* 3 + '🌲' * 4 + '👀' + '🎋' + '🌲' + ' ' + '➡' + '✨' + '💰' + '✨' + ' ' + '➡' + '🌲' * 2 + '🌸' * 3 + '🐸' + '🌺' * 2 + ' ' + '➡'* 3  + ' '
+    return path3
 
 
 def display_game_route(avatar):
-    input(f"\n{'Game Route:'.center(70)}\n\
-            \n{'S'.center(13)}\n\
-            \n{display_first_line_route().center(70)}\n\
-            \n{avatar.img}{display_second_line_route().center(68)}\n\
-            \n{display_third_line_route().center(70)}\n\
-            \n{'T'.center(13)}\n\
+    print('\033c')
+    input(f"\n{'Game Route:'.center(70)}\
+            \n{'~'*70}\
+            \n   S\n\
+            \n   T{display_first_line_route()} E\n\
+            \n{avatar.img}  A{display_second_line_route()} N\n\
+            \n   R{display_third_line_route()} D\n\
+            \n   T\n\
             \n\n{'Press enter to continue'.ljust(70)}\n\
         \n")
-
-
+   
+    
 def display_menu():
+    print('\033c')
     valid_input = False
     while not valid_input:
         user_choice = input(f"\n 🎮  Game Menu  📜\
@@ -248,20 +281,25 @@ def choose_avatar():
             valid_input = True
             for avatar_obj in AVATAR_LIST:
                 if avatar_obj.name.lower().startswith(user_choice):
-                    print(f"\nYou have chosen to be a {avatar_obj.img}\n")
+                    input(f"\nYou have chosen to be a {avatar_obj.img}\n\
+                            \nPress enter to continue\n\
+                        ")
                     return avatar_obj
         else:
             print(f"\nPlease enter from the following only: t, l, b, f\n")
 
 
 def display_action_menu(monster):
+    print('\033c')
+    input(f"\nMonster Approaching Grrr!\n\
+                \n {monster.img}  {monster.name} - {monster.description}\n\
+                \n{'-'*70}\n\
+                \nWhat would you like to do?\n\
+                \nPress enter to continue\n\
+            ")
     valid_input = False
     while not valid_input:
-        user_choice =  input(f"\nMonster Approaching Grrr!\n\
-                               \n {monster.img}  {monster.name} - {monster.description}\n\
-                               \n{'-'*70}\n\
-                               \nWhat would you like to do?\n\
-                               \nPress 1 - Attack  🗡 \n\
+        user_choice =  input(f"\nPress 1 - Attack  🗡 \n\
                                \nPress 2 - View stats  🖥 \n\
                                \nPress 3 - Use pick ups 🎁 \n\
                                \nPress 4 - Run away/Move forward 👣 \n\
@@ -275,17 +313,19 @@ def display_action_menu(monster):
 
 
 def display_repeat_action_menu(monster):
+    print('\033c')
     valid_input = False
     while not valid_input:
-        user_choice =  input(f"\n{monster.img}  {monster.name} Turning Nasty! groWWRR! 👿 😡 👿 \
-                               \n{'-'*70}\
-                               \nWhat would you like to do next?\n\
-                               \nPress 1 - Attack  🗡 \n\
+        print(f"\n{monster.img}  {monster.name} Turning Nasty! groWWRR! 👿 😡 👿 \
+                \n{'-'*70}\
+                \nWhat would you like to do next?\n\
+             ")
+        user_choice =  input(f"\nPress 1 - Attack  🗡 \n\
                                \nPress 2 - View stats  🖥 \n\
                                \nPress 3 - Use pick ups 🎁 \n\
                                \nPress 4 - Run away/Move forward 👣 \n\
                                \nPress 5 - Exit game  👋 \n\
-                            \n")
+                               \n")
         if re.search('[12345]',user_choice):
             valid_input = True
             return user_choice
@@ -294,10 +334,11 @@ def display_repeat_action_menu(monster):
 
 
 def display_incomplete_journey_message(avatar):
-    print(f"{'Level Completed: '.ljust(20)}0\
+    print('\033c')
+    print(f"{'Journey Incomplete! '.center(20)}\
           \n{'Score: '.ljust(20)}{avatar.score}\
           \n{'Monsters Killed: '.ljust(20)}{avatar.monsters_killed}\
-          \n{'Pick-Ups collected: '.ljust(20)}{len(avatar.pick_ups_collected)}\
+          \n{'Pick-Ups collected: '.ljust(20)}{len(avatar.pick_ups_collected)}\n\
          ")
 
 
@@ -324,18 +365,19 @@ def accept_user_choice_of_pick_up(avatar):
 
 
 def display_pick_ups_to_be_used_menu(avatar):
-        no_of_pick_ups_collected = len(avatar.pick_ups_collected)
-        if no_of_pick_ups_collected == 0:
-            print(f"Oops! No pick-ups collected yet!\n\
-                   \nGoing Back!\n\
-                 ")
-        elif len(avatar.pick_ups_collected) == len(avatar.pick_ups_used):
-            print(f"Gosh! All pick-ups have been used up!\n\
-                   \nGoing Back!\n\
-                 ")
-        else:
-            user_choice = int(accept_user_choice_of_pick_up(avatar))
-            avatar.decide_to_use_pick_ups(user_choice)
+    print('\033c')
+    no_of_pick_ups_collected = len(avatar.pick_ups_collected)
+    if no_of_pick_ups_collected == 0:
+        input(f"Oops! No pick-ups collected yet!\n\
+                \nPress enter to go Back!\n\
+            ")
+    elif len(avatar.pick_ups_collected) == len(avatar.pick_ups_used):
+        input(f"Gosh! All pick-ups have been used up!\n\
+                \nPress enter to go Back!\n\
+            ")
+    else:
+        user_choice = int(accept_user_choice_of_pick_up(avatar))
+        avatar.decide_to_use_pick_ups(user_choice)
                 
 
 def assign_actions(action,avatar,monster):
@@ -349,7 +391,7 @@ def assign_actions(action,avatar,monster):
         avatar.decide_to_run(monster)
     if action == "5":
         avatar.quit_game = True
-
+    
 
 def react_to_monster(avatar, monster):
     action = display_action_menu(monster)
@@ -363,52 +405,55 @@ def react_to_monster(avatar, monster):
             break
         action = display_repeat_action_menu(monster)
 
+
 def check_if_pick_ups_available(avatar):
+    print('\033c')
     no_of_pick_ups_collected = len(avatar.pick_ups_collected)
-    if no_of_pick_ups_collected < 3:
+    if no_of_pick_ups_collected < 2:
         if avatar.steps >= MILESTONES_FOR_PICK_UP_LIST[no_of_pick_ups_collected]:
             latest_pick_up = PICK_UP_LIST[no_of_pick_ups_collected]
             avatar.pick_ups_collected.append(latest_pick_up)
             avatar.score += 5
-            print(f"Well Done! You've collected a {latest_pick_up.img}  {latest_pick_up.name} - {latest_pick_up.description}\n")
+            input(f"Well Done! You've collected a {latest_pick_up.img}  {latest_pick_up.name}\n\
+                   \n✨ When used will help to - {latest_pick_up.description} ✨\n\
+                   \nPress enter to continue\n\
+                 ")
     
  
 def roll_dice_to_move_forward(avatar):
-    input(f"\nMove forward - 👉  \n\
-            \nTread slowly!  🐾   🐾 \
-            \nPay attention to the tangled vines, lest they gobble you! 🌳🎋🌲🌳🌲🎋  👀 \
-            \nRoll a dice to move forward that many steps.  🎲 \
-            \nPress enter to roll now:\n\
-         ")
+    print('\033c')
     no_of_steps_forward = random.randint(1,6)
     avatar.steps += no_of_steps_forward
-    print(f"\nYou moved {no_of_steps_forward} steps.\n")
     avatar.moved_forward = True
-  
-
+    input(f"You moved {no_of_steps_forward} steps. 🐾\n\
+            \nPress enter to continue\n\
+         ")
+    
+   
 def end_of_journey(avatar):
+    print('\033c')
     print(f"✨  Hurrah! ✨  You've made it through the maze of monsters!  🎇 \n\
-            \nYour score is {avatar.score}  ✨\n\
-            \nYou slayed {avatar.monsters_killed} monsters   ⚔ 🗡 †\n\
-            \nYou collected {len(avatar.pick_ups_collected)} items  🎁\n\
+            \n{'Score:'.ljust(20)} {avatar.score}  ✨\n\
+            \n{'Monsters Slayed:'.ljust(20)} {avatar.monsters_killed}  🗡\n\
+            \n{'Pick-Ups Collected:'.ljust(20)} {len(avatar.pick_ups_collected)}  🎁\n\
         ")
-    if avatar.monsters_killed <= 2:
-        print(f"Level Completed: 1 - Lucky to be alive!  😰")
-    elif avatar.monsters_killed > 2 and avatar.monsters_killed <= 5:
-        print(f"Level Completed: 2 - The future awaits you! 😎")
-    elif monsters_killed > 5:
-        print(f"Level Completed: 3 - Brave Warrior, our Salutations! 🙇")
+    if avatar.monsters_killed == 1:
+        print(f"\nLevel 1: Lucky to be alive!  😰\n")
+    elif avatar.monsters_killed == 2 and avatar.monsters_killed <= 4:
+        print(f"\nLevel 2: Future awaits you! 😎\n")
+    elif avatar.monsters_killed > 2:
+        print(f"\nLevel 3: Brave Warrior, Salutations! 🙇\n")
 
 
 def choose_random_monster(avatar):
     monster = random.choice(MONSTERS_LIST)
     while monster.name in avatar.monsters_encountered:
         if len(avatar.monsters_encountered) == len(MONSTERS_LIST):
-            #print("monsters total = monsters encountered, resetting monsters_encountered\n") 
             monsters_encountered = [] 
         monster = random.choice(MONSTERS_LIST)
     
     return monster
+
 
 def play_game():
     avatar = AVATAR_LIST[0]
@@ -438,19 +483,19 @@ def start_game(avatar):
         
         if avatar.HP <= 0 or avatar.quit_game == True:
             display_incomplete_journey_message(avatar)
-            exit_game()
             break
         
         if avatar.moved_forward == False:
             roll_dice_to_move_forward(avatar)
         check_if_pick_ups_available(avatar)
 
-        if avatar.steps >= 20:
+        if avatar.steps >= 12:
             end_of_journey(avatar)
             break
     
 
 def exit_game():
+    print('\033c')
     print(f"\nUntil next time, goodbye!")
     return True
     
